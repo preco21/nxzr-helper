0048da745720e18e82ff15384cdf97fa123be2aa
Plusb Preco <plusb21@gmail.com>
2022-10-10 02:01:11 +0900

    feat: add supplying env variables to build-kernel script

--- scripts/build-kernel.sh
+++ scripts/build-kernel.sh
@@ -11,18 +11,41 @@ if [ ! -d "WSL2-Linux-Kernel" ]; then
   exit 1
 fi
 
-if [ ! -f ".config" ]; then
-  echo "'.config' file does not exist. Did you run the 'prepare' script?"
-  exit 1
-fi
-
 pushd WSL2-Linux-Kernel/
 
+# Export required kconfig settings for building.
+cat <<EOF >.config-fragment
+CONFIG_BT=y
+CONFIG_BT_BREDR=y
+CONFIG_BT_LE=y
+CONFIG_BT_DEBUGFS=y
+CONFIG_BT_INTEL=y
+CONFIG_BT_BCM=y
+CONFIG_BT_RTL=y
+CONFIG_BT_HCIBTUSB=y
+CONFIG_BT_HCIBTUSB_BCM=y
+CONFIG_BT_HCIBTUSB_RTL=y
+CONFIG_BT_HCIUART=y
+CONFIG_PREVENT_FIRMWARE_BUILD=n
+CONFIG_EXTRA_FIRMWARE="rtl_bt/rtl8761bu_fw.bin"
+CONFIG_EXTRA_FIRMWARE_DIR="/lib/firmware"
+EOF
+
+# Copy the defualt `kconfig` to `.config`.
+echo "> Copying current kconfig to '.config' file..."
+cp /proc/config.gz config.gz && gunzip config.gz
+mv config .config
+
+echo "> Merging '.config-fragment' into current '.config'..."
+./scripts/kconfig/merge_config.sh .config .config-fragment
+
 # Run the build.
+echo "> Running the build..."
 N_CORES=$(getconf _NPROCESSORS_ONLN)
 make -j $N_CORES && make modules_install -j $N_CORES && make install -j $N_CORES
 
 # Copy the built kernel to `kernel-dist` folder in project root.
+echo "> Copying the resulting kernel image..."
 cp arch/x86/boot/bzImage ../kernel-dist/nxzr-bzImage
 
 popd
